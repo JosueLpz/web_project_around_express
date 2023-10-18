41ce1b5c1bd6b927af449af1ceb5954c89e4c98e
unknown <josuelpzti@gmail.com>
2023-10-18 09:10:03 -0300

    muestra usuarios

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,30 @@
 const router = require("express").Router();
-const usersData = require("../data/users.json");
+const fs = require("fs");
+const path = require("path");
 
-router.get("/users", (req, res) => {
-  res.send(usersData);
+const userPath = path.join(__dirname, "../data/users.json");
+
+router.get("/", (req, res) => {
+  fs.readFile(userPath, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      console.error("Error al leer el archivo:", err);
+      res.status(500).send("Ha ocurrido un error, intente de nuevo más tarde");
+      return;
+    }
+    res.send(data);
+  });
 });
 
-router.get("/users/:_id", (req, res) => {
-  const { _id } = req.params;
+// router.get("/:id", (req, res) => {
+//   const { id } = req.params;
 
-  const user = usersData.find((user) => user._id === _id);
+//   const user = usersData.find((user) => user._id === id);
 
-  if (!user) {
-    res.send({ error: `Este usuario no existe` });
-    return;
-  }
-  res.send(user);
-});
+//   if (!user) {
+//     res.send({ error: `Este usuario no existe` });
+//     return;
+//   }
+//   res.send(user);
+// });
 
 module.exports = router;
